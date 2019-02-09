23674a3e8dad94b254dfd8e59f88c32573d4b01c
Chang Wang <garnwraly@gmail.com>
2019-02-08 21:11:50 -0500

    print warning when package doesn't contain scripts

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 #!/usr/bin/env node
-const readPkgUp = require("read-pkg-up");
+const readPkgUp = require('read-pkg-up');
 const packageInfo = readPkgUp.sync();
 
 const package = packageInfo.pkg;
 
+if (
+  !package.scripts ||
+  typeof package.scripts !== 'object' ||
+  Object.keys(package.scripts).length === 0
+) {
+  console.log(`Package does not contain 'scripts'\n  ${packageInfo.path}`);
+  process.exit();
+}
+
 Object.entries(package.scripts).forEach(([key, script]) => {
-  console.log(`${key} => ${script}`)
-})
+  console.log(`${key} => ${script}`);
+});
